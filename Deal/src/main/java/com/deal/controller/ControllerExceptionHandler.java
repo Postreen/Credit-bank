fda69a2a5ed3444fc57123e7478046765ed0cab3
Posteen <artem2211.s@mail.ru>
2024-12-25 12:25:36 +0400

    bugfix/deal/rollback changes

--- Deal/src/main/java/com/deal/controller/ControllerExceptionHandler.java
+++ Deal/src/main/java/com/deal/controller/ControllerExceptionHandler.java
@@ -29,7 +29,7 @@ public class ControllerExceptionHandler {
         String message = e.getBindingResult().getFieldErrors().stream()
                 .map(error -> error.getField() + ": " + error.getDefaultMessage())
                 .collect(Collectors.joining(", "));
-        log.error("Error = {}", message);
+        log.error("Validation error DTO, message: {}", message);
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .body(new ErrorMessageDto(message));
@@ -46,12 +46,11 @@ public class ControllerExceptionHandler {
     @ExceptionHandler(ScoringException.class)
     public ResponseEntity<ErrorMessageDto> handlerScoringException(ScoringException e) {
         String message = e.getMessage();
-        log.debug("ScoringException occurred: {}", message);
+        log.error("ScoringException occurred: {}", message);
 
         return ResponseEntity
                 .status(HttpStatus.UNPROCESSABLE_ENTITY)
                 .body(new ErrorMessageDto(message));
     }
-
 }
 
